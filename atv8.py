efac277e34990afb007a5dfd6c29bb89754ea6e7
Bigpessoa07 <grandejppessoa07@gmail.com>
2025-03-29 22:24:50 -0300

    Update atv8.py

--- atv8.py
+++ atv8.py
@@ -1,7 +1,5 @@
-#Solicite uma temperatura em Celsius e converta para Fahrenheit.
+celsius = int(input("Diga uma temperatura em Celsius: "))
 
-temp = int(input("Diga uma temperatura em Celsius: "))
-
-conv = (temp * 9/5) + 32
+conv = (celsius * 9/5) + 32
 
 print(f"Temperatua convertida: {conv}")
